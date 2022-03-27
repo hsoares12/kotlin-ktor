245c6465f5246b96bf30eb2fb3be33e14de46f4d
huguescarlos.soares@gmail.com <huguescarlos.soares@gmail.com>
2022-03-26 23:20:52 -0400

    initial commit

---
+++ src/test/kotlin/com/soaz/ApplicationTest.kt
@@ -0,0 +1,22 @@
+package com.soaz
+
+import com.soaz.plugins.configureRouting
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
